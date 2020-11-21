a69d546ff26a4d4ef911a814f21e7fb21dea1fdc
adriangohjw <adriangohjw@gmail.com>
2020-11-21 11:28:32 +0800

    Add SQL script for populating DB

---
+++ assignment2/myproject/src/main/webapp/sql/Person.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA CZ3002;
+
+CREATE  TABLE CZ3002.person (
+  id INT NOT NULL ,
+  name VARCHAR(45) NOT NULL ,
+  password VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (id) ,
+  UNIQUE INDEX PERSON_NAME_UNIQUE (name ASC));
+
+INSERT INTO CZ3002.person VALUES (1, 'adrian', 'secret');
